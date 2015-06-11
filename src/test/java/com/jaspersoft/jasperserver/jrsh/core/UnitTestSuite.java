dcbe7a13353144b641679f3da063f200cb3f1b3a
Krasnyanskiy <krasnyanskiy.alex@gmail.com>
2015-06-11 16:05:28 +0300

    Minor changes

--- src/test/java/com/jaspersoft/jasperserver/jrsh/core/UnitTestSuite.java
+++ src/test/java/com/jaspersoft/jasperserver/jrsh/core/UnitTestSuite.java
@@ -1,11 +1,12 @@
 package com.jaspersoft.jasperserver.jrsh.core;
 
+import com.jaspersoft.jasperserver.jrsh.core.operation.parser.ConditionsTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    //ConditionsTest.class
+    ConditionsTest.class
 })
 public class UnitTestSuite {
 }
